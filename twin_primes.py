85edc779aecfbee2a3be2bf714e2e844d3589663
russjohnson09 <russjohnson09@gmail.com>
2012-09-08 17:53:16 -0400

    init

---
+++ twin_primes.py
@@ -0,0 +1,53 @@
+# Sieve of Eratosthenes
+# Code by David Eppstein, UC Irvine, 28 Feb 2002
+# http://code.activestate.com/recipes/117119/
+
+def gen_primes():
+    """ Generate an infinite sequence of prime numbers.
+    """
+    # Maps composites to primes witnessing their compositeness.
+    # This is memory efficient, as the sieve is not "run forward"
+    # indefinitely, but only as long as required by the current
+    # number being tested.
+    #
+    D = {}  
+
+    # The running integer that's checked for primeness
+    q = 2  
+
+    while True:
+        if q not in D:
+            # q is a new prime.
+            # Yield it and mark its first multiple that isn't
+            # already marked in previous iterations
+            # 
+            yield q        
+            D[q * q] = [q]
+        else:
+            # q is composite. D[q] is the list of primes that
+            # divide it. Since we've reached q, we no longer
+            # need it in the map, but we'll mark the next 
+            # multiples of its witnesses to prepare for larger
+            # numbers
+            # 
+            for p in D[q]:
+                D.setdefault(p + q, []).append(p)
+            del D[q]
+
+        q += 1
+
+a = gen_primes()
+
+lst = []
+
+for i in range(1000):
+    lst.append(a.next())
+    
+counter = 1
+for i in lst:
+    if i + 2 in lst:
+        print str(counter) + ".\t" + str((i,i+1))
+        counter += 1
+        
+        
+    
